1d2c8cf9baab4a73dff7d53b7e8daa45e3946192
Dmitry Lihachev <lda@openteam.ru>
2012-01-17 15:50:32 +0700

    заработало удаление прав доступа

--- app/controllers/esp_permissions/permissions_controller.rb
+++ app/controllers/esp_permissions/permissions_controller.rb
@@ -2,12 +2,12 @@ class EspPermissions::PermissionsController < EspPermissions::ApplicationControl
   belongs_to :user, :shallow => true
   actions :new, :create, :destroy
 
-  def create
-    create!{ [:manage, :permissions, :users]}
-  end
+  #def create
+    #create!{  :users]}
+  #end
 
-  def destroy
-    destroy!{ [:manage, :permissions, :users] }
-  end
+  #def destroy
+    #destroy!{ [:manage, :permissions, :users] }
+  #end
 
 end
