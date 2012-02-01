c6baf26eed0fa979d3cb5386e0bb5d0abf94df26
Dmitry Lihachev <lda@openteam.ru>
2012-02-01 09:15:24 +0700

    title для subcontext

--- lib/generators/esp_auth/install/templates/app/models/subcontext.rb
+++ lib/generators/esp_auth/install/templates/app/models/subcontext.rb
@@ -2,7 +2,10 @@ class Subcontext < ActiveRecord::Base
   belongs_to :context
   has_many :permissions, :as => :context
 
+  alias_attribute :to_s, :title
+
   def depth
     context.depth + 1
   end
+
 end
