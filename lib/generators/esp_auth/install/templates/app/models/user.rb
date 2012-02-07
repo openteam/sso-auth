7badb8228f9e73cd2eae4be78d418012e5208fcb
Dmitry Lihachev <lda@openteam.ru>
2012-02-07 11:57:12 +0700

    simplify user model

--- lib/generators/esp_auth/install/templates/app/models/user.rb
+++ lib/generators/esp_auth/install/templates/app/models/user.rb
@@ -1,39 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :nickname, :name, :first_name, :last_name, :location, :description, :image, :phone, :urls, :raw_info, :uid
 
-  validates_presence_of :uid
-
-  has_many :permissions
-
-  default_value_for :sign_in_count, 0
-
-  devise :omniauthable, :trackable, :timeoutable
-
-  searchable do
-    integer :uid
-    text :term do [name, email, nickname].join(' ') end
-    integer :permissions_count do permissions.count end
-  end
-
-  Permission.enums[:role].each do | role |
-    define_method "#{role}_of?" do |context|
-      permissions.for_role(role).for_context_and_ancestors(context).exists?
-    end
-    define_method "#{role}?" do
-      permissions.for_role(role).exists?
-    end
-  end
-
-  def contexts
-    permissions.map(&:context).uniq
-  end
-
-  def contexts_tree
-    contexts.flat_map{|c| c.respond_to?(:subtree) ? c.subtree : c}
-            .uniq
-            .flat_map{|c| c.respond_to?(:subcontexts) ? [c] + c.subcontexts : c }
-            .uniq
-  end
+  esp_authable
 
 end
 
