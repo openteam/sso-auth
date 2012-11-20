08033f19025157df778e954db839247e6e7dbb1d
Dmitry Lihachev <lda@openteam.ru>
2012-11-20 12:29:45 +0700

    remove users_controller; routes: simplify; umount engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
-SsoAuth::Engine.routes.draw do
-  resources :users, :only => :index
-  get 'sign_out' => 'sessions#destroy', :as => :destroy_user_session
-end
-
 Rails.application.routes.draw do
   devise_for :users, :path => 'auth',
                      :controllers => {:omniauth_callbacks => 'sso_auth/omniauth_callbacks'},
                      :skip => [:sessions]
 
   devise_scope :users do
+    get 'sign_out' => 'sso-auth/sessions#destroy', :as => :destroy_user_session
     get 'sign_in' => redirect('/auth/auth/identity'), :as => :new_user_session
   end
-
-  mount SsoAuth::Engine => '/auth'
 end
 
